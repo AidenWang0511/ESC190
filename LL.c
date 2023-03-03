ea1fa1ac2717b69064739717c0f9ddb904baeca4
AidenWang0511 <70038087+AidenWang0511@users.noreply.github.com>
2023-03-03 08:13:37 -0500

    finished link list started array list

--- LL.c
+++ LL.c
@@ -54,16 +54,56 @@ void LL_append(LL *my_list, int new_elem)
     my_list->size++;
 }
 
-//insert new_elem at index index in linked list my_list
 void LL_insert(LL *my_list, int new_elem, int index)
 {
-   
+    // TODO
+    //insert new_elem at index index in linked list my_list
+
+    node *n = (node*)malloc(sizeof(node));
+    n->data = new_elem;
+
+    if(index < 0 || index >= my_list->size){
+        printf("insert: Index out of bound!!!!!!\n");
+        return;
+    }else if(index == 0){
+        n->next = my_list->head;
+        my_list->head = n;
+    }else{
+        node *prev = my_list->head;
+        for(int i=0; i<index-1; i++){
+            prev = prev->next;
+        }
+        n->next = prev->next;
+        prev->next = n;
+    }
+    my_list->size++;
 }
 
 void LL_delete(LL *my_list, int index)
 {
     // TODO
     // Delete element at index index in linked list my_list
+
+    node *n_delete;
+
+    if(index < 0 || index >= my_list->size){
+        printf("remove: Index out of bound!!!!!!\n");
+        return;
+    }else if(index == 0){
+        n_delete = my_list->head;
+        my_list->head = n_delete->next;
+    }else{
+        node *prev = my_list->head;
+        for(int i=0; i<index-1; i++){
+            prev = prev->next;
+        }
+        n_delete = prev->next;
+        prev->next = prev->next->next;
+    }
+    
+    free(n_delete);
+    my_list->size--;
+
 }
 
 void LL_free_all(LL *my_list)
@@ -71,8 +111,27 @@ void LL_free_all(LL *my_list)
     // TODO
     // Free all memory allocated for linked list my_list,
     // including all the nodes
+    node *cur = my_list->head, *nxt;
+
+    while(cur != NULL){
+        nxt = cur->next;
+        free(cur);
+        cur = nxt;
+    }
+
+    my_list->head = NULL;
+    my_list->size = 0;
+    //not sure if it's the above one or below
+    //free(my_list);
 }
 
+int LL_get_rec_helper(node *cur, int index){
+    if(index == 0){
+        return cur->data;
+    }else{
+        return LL_get_rec_helper(cur->next, index-1);
+    }
+}
 
 int LL_get_rec(LL *my_list, int index)
 {
@@ -80,6 +139,13 @@ int LL_get_rec(LL *my_list, int index)
     // Return the element at index index in linked list my_list
     // Use recursion. you must not use for, while, or do-while loops
     // You should use a helper function
+    if(index < 0 || index >= my_list->size){
+        printf("remove: Index out of bound!!!!!!\n");
+        return -1;
+    }else{
+        return LL_get_rec_helper(my_list->head, index);
+    }
+
 }
 
 typedef struct ArrayList{
@@ -91,11 +157,19 @@ typedef struct ArrayList{
 void create_AL_from_data(ArrayList **p_AL, int *data_arr, int size)
 {
     // TODO
+    *p_AL = (p_AL*)malloc(sizeof(ArrayList));
+    (*p_AL)->size, (*p_AL->capacity) = size;
+    (*p_AL)->data = (int*)malloc(sizeof(int)*size);
+    for(int i=0; i<size; i++){
+        (*p_AL)->data[i] = data_arr[i];
+    }
+
 }
 
 void AL_append(ArrayList *my_list, int new_elem)
 {
     // TODO
+    
 }
 
 void AL_insert(ArrayList *my_list, int new_elem, int index)
@@ -125,5 +199,33 @@ int main()
         printf("%d\n", cur->data);
         cur = cur->next;
     }
+
+    printf("Test for LL_insert:\n");
+    LL_insert(my_list, 7, 2);
+    cur = my_list->head;
+    while(cur != NULL){
+        printf("%d\n", cur->data);
+        cur = cur->next;
+    }
+
+    printf("Test for LL_delete:\n");
+    LL_delete(my_list, my_list->size - 1);
+    cur = my_list->head;
+    while(cur != NULL){
+        printf("%d\n", cur->data);
+        cur = cur->next;
+    }
+
+    printf("Test for LL_get_rec:\n");
+    printf("value at index 2: %d\n", LL_get_rec(my_list, 2));
+
+    printf("Test for LL_free_all:\n");
+    LL_free_all(my_list);
+    cur = my_list->head;
+    while(cur != NULL){
+        printf("%d\n", cur->data);
+        cur = cur->next;
+    }
+    
     return 0;
 }
